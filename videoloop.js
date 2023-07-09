bdcdc0c010c1ee0ac0e669f713bc514e8d2c879e
sngweizhi <47587454+sngweizhi@users.noreply.github.com>
2023-07-09 13:44:41 -0400

    Update videoloop.js

--- videoloop.js
+++ videoloop.js
@@ -1,13 +1,13 @@
-var parentDivs = document.querySelectorAll('.notion-video'); // Selects all elements with the class 'notion-video'
+var parentDivs = document.querySelectorAll('.notion-video'); 
 parentDivs.forEach(parentDiv => {
-  var video = parentDiv.querySelector('video'); // Selects the 'video' tag inside the current 'notion-video' element
-  if (video) { // Check if the video element exists
-    video.loop = true; // Sets the video to loop once it ends
-    video.autoplay = true; // Sets the video to autoplay once it's loaded
-    video.setAttribute("muted", ""); // Mutes the video
-    video.setAttribute('playsinline', ''); // Allows the video to play inline on iOS devices
-    video.setAttribute('webkit-playsinline', ''); // Allows the video to play inline on iOS devices with the 'webkit' prefix
-    video.removeAttribute("controls"); // Removes the video controls
-    video.load(); // Loads the video
+  var video = parentDiv.querySelector('video'); 
+  if (video) { 
+    video.loop = true;
+    video.autoplay = true; 
+    video.setAttribute("muted", ""); 
+    video.setAttribute('playsinline', ''); 
+    video.setAttribute('webkit-playsinline', '');
+    video.removeAttribute("controls"); 
+    video.load(); 
   }
 });
