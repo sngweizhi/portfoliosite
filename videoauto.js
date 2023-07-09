6520ab36dd0a64da1b3a665aba0b81e13e641905
sngweizhi <47587454+sngweizhi@users.noreply.github.com>
2023-07-09 14:39:55 -0400

    Add files via upload

---
+++ videoauto.js
@@ -0,0 +1,12 @@
+var parentDivs = document.querySelectorAll('.notion-video'); 
+parentDivs.forEach(parentDiv => {
+  var video = parentDiv.querySelector('video'); 
+  if (video) { 
+    video.loop = true;
+    video.autoplay = true; 
+    video.setAttribute("muted", ""); 
+    video.setAttribute('playsinline', ''); 
+    video.setAttribute('webkit-playsinline', ''); 
+    video.load(); 
+  }
+});
