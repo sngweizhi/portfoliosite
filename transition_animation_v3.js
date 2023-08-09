7ba64186e816670604a5353cdfc7acd337e71133
sngweizhi <47587454+sngweizhi@users.noreply.github.com>
2023-08-09 13:09:23 -0400

    Update transition_animation_v3.js

--- transition_animation_v3.js
+++ transition_animation_v3.js
@@ -15,33 +15,7 @@ function handleIntersect(entries, observer) {
 }
 
 function animateOnScroll() {
-  const a_blocks = [
-    // Hero
-    { id: "block-160e419fb1e6438f9a4ed9033e4e92b6", class: "reveal-1" },
-    { id: "block-6045708d56d7467a888de67ac78a2315", class: "reveal-2" },
-    { id: "block-eb41dcba0a784bb9ba8ff77f1451bd20", class: "reveal-3" },
-    { id: "block-f667b297baec4a138de37dcc008fd046", class: "reveal-3" },
-    { id: "block-c36e69ea2e1e406393db07719790f5d6", class: "reveal-2" },
-    { id: "block-3360bfc3d1d841239559c52e570998c2", class: "reveal-1" },
-    
-    //Projects
-    { id: "block-b152895395ff4dde8b536916aa4f1374", class: "reveal-4" },
-    
-    //Know more
-    { id: "block-240fe9514f7a49d1be9397abcaa415d1", class: "reveal-1" },
-    { id: "block-9b1df09f344147bd9e1dd5d950accda9", class: "reveal-1" },
-    { id: "block-1c94a31365dd4c92b3b3387070f0fa92", class: "reveal-2" },
-    { id: "block-20fdf2b5bbfe4ed291d0f037beb298e1", class: "reveal-2" },
-    { id: "block-5a7ddeb4873445eabc2b095c9d1728e3", class: "reveal-2" },
-  ];
-  
-  a_blocks.forEach((block) => {
-    const el = document.querySelector(`#${block.id}`);
-    if (el) {
-      el.classList.add('reveal-on'); // Ensure the elements are initially hidden
-      el.classList.add(block.class);
-    }
-  });
+
 
   const observer = new IntersectionObserver(handleIntersect, options);
   const targets = document.querySelectorAll(".reveal-on");
